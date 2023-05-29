414c6db7ebd04f379e2e55529375db82b30ef49d
fernandogtorrez <fernandogtorrez@gmail.com>
2023-05-29 10:02:12 -0500

    modificando archivo funciones

--- funciones.js
+++ funciones.js
@@ -1,7 +1,7 @@
 const arrayDeObjetos = [
     { name: "Jose", edad: 2 },
     { name: "Jose", edad: 2 },
-    { name: "Jose", edad: 2 }
+    { name: "Fernando", edad: 27 }
   ];
 
 function sumar(a,b){
@@ -10,4 +10,4 @@ function sumar(a,b){
 
 sumar(2,5)
 
-  
+const restar = (a,b) => a - b
